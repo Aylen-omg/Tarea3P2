40af29f985278824e2eaad69fbc4f4907fddd2c7
Aylen-omg <82118099+Aylen-omg@users.noreply.github.com>
2021-06-24 23:42:57 -0400

    Update Caracol.java

--- Matrices/Caracol.java
+++ Matrices/Caracol.java
@@ -4,33 +4,33 @@ public class Caracol
     int num;
     int[][]numeros;
     public void DarTamanio(int num){
-        this.num=num;
-        numeros=new int[num][num];
+        this.num = num;
+        numeros = new int[num][num];
     }
 
     public void Ordenar(){
-        int cont=1;
-        int fila=0;
-        int colum=0;
-        for(int i=0;i<num;i++){
-            colum=i;
-            for(fila=i;fila<=(num-1-i);fila++){
-                numeros[colum][fila]=cont;
+        int cont = 1;
+        int fila = 0;
+        int colum = 0;
+        for(int i=0; i<num; i++){
+            colum = i;
+            for(fila=i; fila<=(num-1-i); fila++){
+                numeros[colum][fila] = cont;
                 cont++;
             }
-            fila=num-1-i;
+            fila = num-1-i;
             for(colum=i+1;colum<=(num-1-i);colum++){
-                numeros[colum][fila]=cont;
+                numeros[colum][fila] = cont;
                 cont++;
             }
-            colum=num-1-i;
-            for(fila=(num-2-i);fila>=i;fila--){
-                numeros[colum][fila]=cont;
+            colum = num-1-i;
+            for(fila = (num-2-i); fila>=i; fila--){
+                numeros[colum][fila] = cont;
                 cont++;
             }
-            fila=i;
-            for(colum=(num-2-i);colum>=i+1;colum--){
-                numeros[colum][fila]=cont;
+            fila = i;
+            for(colum = (num-2-i); colum>=i+1; colum--){
+                numeros[colum][fila] = cont;
                 cont++;
             }
 
@@ -39,8 +39,8 @@ public class Caracol
 
     public void MostrarMatriz(){
         System.out.println("Matriz de tamaño: "+ num + " x " + num);
-        for(int i=0;i<num;i++){
-            for(int j=0;j<num;j++){
+        for(int i=0; i<num; i++){
+            for(int j=0; j<num; j++){
                 if(numeros[i][j]<10){
                     System.out.print("0"+numeros[i][j]+" ");
                 }else{
